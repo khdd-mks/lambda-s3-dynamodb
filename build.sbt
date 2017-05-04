ef9f60939e23d71b361ece7eeda73e708c8a7e5c
khdd_mks <hisashi.kitajima@gmail.com>
2017-05-04 11:18:56 +0900

    first commit

---
+++ build.sbt
@@ -0,0 +1,19 @@
+name := "lambda-s3-dynamodb"
+
+version := "1.0"
+
+scalaVersion := "2.12.1"
+
+// Change this to another test framework if you prefer
+libraryDependencies ++= Seq(
+  "com.amazonaws" % "aws-java-sdk-s3" % "1.11.125",
+  "com.amazonaws" % "aws-lambda-java-core" % "1.1.0",
+  "com.amazonaws" % "aws-lambda-java-events" % "1.3.0",
+  "com.typesafe" % "config" % "1.3.1"
+  //"org.scalatest" %% "scalatest" % "2.2.4" % "test"
+)
+
+// Uncomment to use Akka
+//libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.3.11"
+
+//unmanagedClasspath in Runtime += baseDirectory.value / "conf"
---
+++ src/main/scala/jp/ne/khddmks/Main.scala
@@ -0,0 +1,43 @@
+package jp.ne.khddmks
+
+import java.io.{BufferedReader, InputStreamReader}
+
+import com.amazonaws.regions.Regions
+import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.lambda.runtime.events.S3Event
+import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
+import com.typesafe.config.ConfigFactory
+
+import scala.util.control.NonFatal
+
+/**
+  * Created by khdd-mks on 2017/05/02.
+  */
+class Main {
+  def handler(s3Event: S3Event, context: Context): Unit = {
+    def createS3 = {
+      val s3 = AmazonS3ClientBuilder.standard()
+        .withRegion(Regions.AP_NORTHEAST_1)
+        .build()
+      s3
+    }
+    def readFromS3(s3: AmazonS3) = {
+      import scala.collection.JavaConverters._
+      val (bucketInfo, objectInfo) = {
+        val temp = s3Event.getRecords.asScala.head.getS3
+        (temp.getBucket, temp.getObject)
+      }
+      val conf = ConfigFactory.load(ConfigFactory.parseReader(
+        new BufferedReader(new InputStreamReader(s3.getObject(bucketInfo.getName, objectInfo.getKey).getObjectContent, "UTF-8"))
+      ))
+      conf
+    }
+
+    try {
+      val conf = readFromS3(createS3)
+      println(conf)
+    } catch {
+      case NonFatal(e) => println(s"エラーが発生しました。設定ファイルが正しいか確認してください。: ${e}")
+    }
+  }
+}
