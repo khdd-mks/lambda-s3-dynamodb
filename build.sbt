d948f389e14e753c3f96eab620d55199b2f43926
khdd_mks <hisashi.kitajima@gmail.com>
2017-05-05 11:06:29 +0900

    write config to DynamoDB

--- build.sbt
+++ build.sbt
@@ -7,6 +7,7 @@ scalaVersion := "2.12.1"
 // Change this to another test framework if you prefer
 libraryDependencies ++= Seq(
   "com.amazonaws" % "aws-java-sdk-s3" % "1.11.125",
+  "com.amazonaws" % "aws-java-sdk-dynamodb" % "1.11.125",
   "com.amazonaws" % "aws-lambda-java-core" % "1.1.0",
   "com.amazonaws" % "aws-lambda-java-events" % "1.3.0",
   "com.typesafe" % "config" % "1.3.1"
--- src/main/scala/jp/ne/khddmks/Main.scala
+++ src/main/scala/jp/ne/khddmks/Main.scala
@@ -3,10 +3,12 @@ package jp.ne.khddmks
 import java.io.{BufferedReader, InputStreamReader}
 
 import com.amazonaws.regions.Regions
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
+import com.amazonaws.services.dynamodbv2.document.{DynamoDB, Item}
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.events.S3Event
 import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
-import com.typesafe.config.ConfigFactory
+import com.typesafe.config.{ConfigFactory, ConfigRenderOptions}
 
 import scala.util.control.NonFatal
 
@@ -22,10 +24,28 @@ class Main {
       s3
     }
     def readFromS3(s3: AmazonS3, bucket: String, key: String) = {
-      val conf = ConfigFactory.load(ConfigFactory.parseReader(
+//      // システムプロパティを含むConfig
+//      val conf = ConfigFactory.load(ConfigFactory.parseReader(
+//        new BufferedReader(new InputStreamReader(s3.getObject(bucket, key).getObjectContent, "UTF-8"))
+//      ))
+      // システムプロパティを含まないConfig (プロパティ内で${java.home}のようにシステムプロパティを参照することはできない)
+      val conf = ConfigFactory.parseReader(
         new BufferedReader(new InputStreamReader(s3.getObject(bucket, key).getObjectContent, "UTF-8"))
-      ))
+      ).resolve
       conf
+    }
+    def createDynamoDB = {
+      val dbClient = AmazonDynamoDBClientBuilder.standard
+        .withRegion(Regions.AP_NORTHEAST_1)
+        .build
+      val db = new DynamoDB(dbClient)
+      db
+    }
+    def writeToDynamoDB(db: DynamoDB, json: String) = {
+      val table = db.getTable("sample001")
+      val item = new Item().withPrimaryKey("key", "config")
+        .withJSON("values", json)
+      table.putItem(item)
     }
 
     try {
@@ -38,6 +58,8 @@ class Main {
       println(s"bucket : ${bucket}, key : ${key}")
       val conf = readFromS3(createS3, bucket, key)
       println(conf)
+      val json = conf.root.render(ConfigRenderOptions.concise)
+      writeToDynamoDB(createDynamoDB, json)
     } catch {
       case NonFatal(e) => println(s"エラーが発生しました。設定ファイルが正しいか確認してください。: ${e}")
     }
